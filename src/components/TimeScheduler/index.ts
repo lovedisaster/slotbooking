04ea2de873da027d8e135f220ddc22ce37afc3eb
Yuefeng Zhang <yuefengzhang@Yuefengs-MacBook-Pro.local>
2025-07-17 20:11:01 +1000

    clear base

--- src/components/TimeScheduler/index.ts
+++
@@ -1,5 +0,0 @@
-import { TimeScheduler } from './TimeScheduler';
-import { TimeSchedulerProvider } from './TimeSchedulerContext';
-
-export { TimeScheduler, TimeSchedulerProvider };
-export * from './mockData';
--- src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Mock Material UI icons
-jest.mock('@mui/icons-material/KeyboardArrowLeft', () => () => 'LeftArrow');
-jest.mock('@mui/icons-material/KeyboardArrowRight', () => () => 'RightArrow');
-
-// Mock date
-jest.useFakeTimers().setSystemTime(new Date('2024-01-15')); 
